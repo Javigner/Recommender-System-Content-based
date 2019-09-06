db0a810c62e7cc6be58551ea791b68aa673ccd64
Jacky VIGNERON <javigner@e1r11p22.42.fr>
2019-09-06 16:49:29 +0200

    dataset

--- recommander_system.py
+++ recommander_system.py
@@ -1,3 +1,8 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
 
 data = pd.read_json("activities_dataset.json")
